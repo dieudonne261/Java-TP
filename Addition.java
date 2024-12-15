f47793d0279a82120686ff7dd4bdc4ad9200cf20
Randrianarison Dieu Donné <ddieu0970@gmail.com>
2024-12-15 19:23:57 +0300

    Java TP

---
+++ Addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n1, n2, somme;
+
+        System.out.print("Donner le premier entier :");
+        n1 = input.nextInt();
+
+        System.out.print("Donner un deuxième entier : ");
+        n2 = input.nextInt();
+
+        somme = n1 + n2;
+
+        System.out.printf("La somme est : %d\n", somme);
+    }
+}
